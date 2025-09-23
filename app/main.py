39210d5526ae42fb04a5791799e1fdebf004cf4c
seonghyeok <chosh2001@naver.com>
2025-09-23 23:05:19 +0900

    refactor: on_event 대신 lifespan을 사용하도록
    on_event는 deprecated

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .scheduler.scheduler import load_arxiv_data_to_dynamodb
 
-app = FastAPI()
 scheduler = AsyncIOScheduler()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 애플리케이션 시작 시 실행
+    scheduler.add_job(load_arxiv_data_to_dynamodb, 'cron', hour=4, minute=0) # 새벽 4시
+    scheduler.start()
+    # 앱 시작 시 즉시 1회 실행 (테스트용)
+    load_arxiv_data_to_dynamodb()
+    
+    yield
+    
+    # 애플리케이션 종료 시 실행
+    scheduler.shutdown()
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def root():
     return {"Hello World!"}
-
-@app.on_event("startup")
-async def startup_event():
-    scheduler.add_job(load_arxiv_data_to_dynamodb, 'cron', hour=4, minute=0) # 새벽 4시
-    scheduler.start()
-    # 앱 시작 시 즉시 1회 실행 (테스트용)
-    load_arxiv_data_to_dynamodb()
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    scheduler.shutdown()
