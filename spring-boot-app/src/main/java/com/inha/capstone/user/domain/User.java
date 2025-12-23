e1eefb4a4258fa61d96c4fbad3713cc6e76ef981
seonghyeok <chosh2001@naver.com>
2025-12-23 16:58:18 +0900

    feat: 사용자 관심사 도메인 및 repository 추가

--- spring-boot-app/src/main/java/com/inha/capstone/user/domain/User.java
+++ spring-boot-app/src/main/java/com/inha/capstone/user/domain/User.java
@@ -5,11 +5,13 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
