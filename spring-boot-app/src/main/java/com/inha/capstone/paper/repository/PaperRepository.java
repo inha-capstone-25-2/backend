3fd9366303a058a70e6e15faf43cb6074940e655
seonghyeok <chosh2001@naver.com>
2025-12-23 18:45:07 +0900

    feat: 논문 도큐먼트 및 레포지토리 구현

---
+++ spring-boot-app/src/main/java/com/inha/capstone/paper/repository/PaperRepository.java
@@ -0,0 +1,9 @@
+package com.inha.capstone.paper.repository;
+
+import com.inha.capstone.paper.model.Paper;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaperRepository extends MongoRepository<Paper, String> {
+}
