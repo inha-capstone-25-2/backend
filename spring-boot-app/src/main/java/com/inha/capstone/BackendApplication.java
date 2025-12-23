8616870115d782bbc7aa017e5a2aff904bab8b80
seonghyeok <chosh2001@naver.com>
2025-12-23 12:22:39 +0900

    feat: JPA 설정 추가

--- spring-boot-app/src/main/java/com/inha/capstone/BackendApplication.java
+++ spring-boot-app/src/main/java/com/inha/capstone/BackendApplication.java
@@ -1,9 +1,10 @@
 package com.inha.capstone;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {
